a25c76380688ba70e2db0b237fcecbe636e8d998
johnbaby <junzhewjz@gmail.com>
2023-03-02 17:09:24 +0800

    first commit

---
+++ DQN.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+import torch
+
+
+class DQN():
+    
+    def main_learning_loop(env):
+        '''
+        env: a gym cartpole enviroment
+        '''
+        
+        
+
+
+
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v1')
